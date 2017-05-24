f3a88ce1b60c7d587f2a39d3c4dae7d144202bb5
VasiliyVikhlyaev <vasiliy.vikhlyaev@outlook.com>
2017-05-24 21:17:00 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
 window.onload = function(){
 	for (var i=0; i<9; i++) {
-		document.getElementById('game').innerHTML+='<div class="block"></div>';
+		document.getElementById('game').innerHTML+='<div class="block"></div>'; <!-- создание сетки зхз -->
 	}
 
 	var hod = 0;
 
-	document.getElementById('game').onclick = function(event){
+	document.getElementById('game').onclick = function(event){ //захват клика по всему блоку
 		console.log(event);
 		if (event.target.className == 'block'){
 			if (hod%2==0) {
@@ -21,7 +21,7 @@ window.onload = function(){
 
 	function checkWinner(){
 		var allblock = document.getElementsByClassName('block');
-		//console.log(allblock);
+		//крестики
 		if (allblock[0].innerHTML=='x' && allblock[1].innerHTML=='x' && allblock[2].innerHTML=='x') alert('Победили крестики');
 		if (allblock[3].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[5].innerHTML=='x') alert('Победили крестики');
 		if (allblock[6].innerHTML=='x' && allblock[7].innerHTML=='x' && allblock[8].innerHTML=='x') alert('Победили крестики');
@@ -39,9 +39,8 @@ window.onload = function(){
 		if (allblock[2].innerHTML=='0' && allblock[5].innerHTML=='0' && allblock[8].innerHTML=='0') alert('Победили нолики');
 		if (allblock[0].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[8].innerHTML=='0') alert('Победили нолики');
 		if (allblock[2].innerHTML=='0' && allblock[4].innerHTML=='0' && allblock[6].innerHTML=='0') alert('Победили нолики');
+		
+		if (hod==9) alert ('Ничья');
 	}
 
 }
-//012
-//345
-//678
